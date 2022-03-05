62a7bce951c2f6d42420dd5b7cd7d3fad7535c20
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-03-05 13:38:57 +0000

    imported logic to build upon

--- src/FindTheSmallest.php
+++ src/FindTheSmallest.php
@@ -4,5 +4,16 @@ namespace App;
 
 class FindTheSmallest
 {
-    
+    public static function smallest($n) {
+        $splitToSort = $splitToWork = str_split($n);
+        sort($splitToSort);
+        $lowest = $splitToSort[0];
+        $indexOfLowestOri = array_search($lowest, $splitToWork);
+        $indexOfLowestNow = array_search($lowest, $splitToSort);
+        unset($splitToWork[$indexOfLowestOri]);
+        array_splice($splitToWork, $indexOfLowestNow, 0, $lowest);
+        $newN = intval(implode('', $splitToWork));
+      //   return $newN;
+        return [$newN, $indexOfLowestOri, $indexOfLowestNow];
+      }
 }
--- tests/FindTheSmallestTest.php
+++ tests/FindTheSmallestTest.php
@@ -5,5 +5,14 @@ use PHPUnit\Framework\TestCase;
 
 class FindTheSmallestTest extends TestCase
 {
-    
+    private function revTest($actual, $expected) {
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**@test */
+    public function test_does_it_work() {
+        $this->revTest(FindTheSmallest::smallest(261235), [126235, 2, 0]);
+        $this->revTest(FindTheSmallest::smallest(209917), [29917, 0, 1]);
+        $this->revTest(FindTheSmallest::smallest(285365), [238565, 3, 1]);
+    }
 }
