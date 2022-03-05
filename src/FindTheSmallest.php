3e58f21dd1ef663d8bda935bb4b89f080ced96c9
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-03-05 14:33:51 +0000

    included task and begun splitting tests

--- src/FindTheSmallest.php
+++ src/FindTheSmallest.php
@@ -17,3 +17,17 @@ class FindTheSmallest
         return [$newN, $indexOfLowestOri, $indexOfLowestNow];
       }
 }
+
+// THE TASK
+
+// You have a positive number n consisting of digits. You can do at most one operation: 
+// Choosing the index of a digit in the number, remove this digit at that index and insert it
+// back to another or at the same place in the number in order to find the smallest number you
+// can get.
+
+// Task:
+// Return an array or a tuple or a string depending on the language (see "Sample Tests") with
+
+// the smallest number you got
+// the index i of the digit d you took, i as small as possible
+// the index j (as small as possible) where you insert this digit d to have the smallest number.
--- tests/FindTheSmallestTest.php
+++ tests/FindTheSmallestTest.php
@@ -10,9 +10,24 @@ class FindTheSmallestTest extends TestCase
     }
 
     // /**@test */
-    // public function test_does_it_work() {
+    // public function it_works() {
     //     $this->revTest(FindTheSmallest::smallest(261235), [126235, 2, 0]);
     //     $this->revTest(FindTheSmallest::smallest(209917), [29917, 0, 1]);
     //     $this->revTest(FindTheSmallest::smallest(285365), [238565, 3, 1]);
     // }
+
+    /**@test */
+    public function test_it_works_when_lowest_digit_is_central() {
+        $this->revTest(FindTheSmallest::smallest(261235)[0], 126235);
+    }
+
+    /**@test */
+    public function test_it_works_when_lowest_digit_is_second() {
+        $this->revTest(FindTheSmallest::smallest(209917)[0], 29917);
+    }
+
+    /**@test */
+    public function test_it_works_when_lowest_digit_is_first() {
+        $this->revTest(FindTheSmallest::smallest(285365)[0], 238565);
+    }
 }
